58b0a78259f21049005a8a316bc83ae1dbc0fd8a
Ali EM <ali.e.m@outlook.com>
2024-04-07 14:44:58 +0200

    Game class declaration

--- src/games.ts
+++ src/games.ts
@@ -1,3 +1,27 @@
+export class GameResume {
+    id: number;
+    title: string;
+    thumbnail: string;
+    short_description: string;
+    genre: string;
+    developer: string;
+    constructor(
+        id: number,
+        title: string,
+        thumbnail: string,
+        short_description: string,
+        genre: string,
+        developer: string
+    ) {
+        this.id = id;
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.short_description = short_description;
+        this.genre = genre;
+        this.developer = developer;
+    }
+}
+
 export const loadGames = async () => {
     const response = await fetch(
         `https://www.freetogame.com/api/games?platform=pc&sort-by=release-date`
