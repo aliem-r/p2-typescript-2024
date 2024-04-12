37eb33fb47651de2b161102525df564ab23f9bb7
aliem-r <ali.e.m@outlook.com>
2024-04-12 20:43:09 +0200

    Game class extended

--- src/games.ts
+++ src/games.ts
@@ -4,23 +4,52 @@ class Game {
     title: string;
     thumbnail: string;
     short_description: string;
+    description: string;
+    game_url: string;
     genre: string;
     developer: string;
+    release_date: string;
+    minimum_requirements: {
+        os: string;
+        processor: string;
+        memory: string;
+        graphics: string;
+        storage: string;
+    };
+    img: string[];
     constructor(
         id: number,
         title: string,
         thumbnail: string,
         short_description: string,
+        description: string,
+        game_url: string,
         genre: string,
-        developer: string
+        developer: string,
+        release_date: string,
+        minimum_requirements: Requirements,
+        img: string[]
     ) {
         this.id = id;
         this.title = title;
         this.thumbnail = thumbnail;
         this.short_description = short_description;
+        this.description = description;
+        this.game_url = game_url;
         this.genre = genre;
         this.developer = developer;
+        this.release_date = release_date;
+        this.minimum_requirements = minimum_requirements;
+        this.img = img;
     }
+}
+
+interface Requirements {
+    os: string;
+    processor: string;
+    memory: string;
+    graphics: string;
+    storage: string;
 }
 
 //load games from freetogame.com to an array of Game instances
@@ -33,13 +62,23 @@ export const loadGames = async (n: number) => {
 
     for (let i = 0; i < n; i++) {
         const result = results[i];
+
+        const detailsResponse = await fetch(
+            `https://www.freetogame.com/api/game?id=${result.id}`
+        );
+        const details: any = await detailsResponse.json();
         const gameInstance = new Game(
             result.id,
             result.title,
             result.thumbnail,
             result.short_description,
+            details.description,
+            result.game_url,
             result.genre,
-            result.developer
+            result.developer,
+            result.release_date,
+            details.minimum_system_requirements,
+            details.screenshots
         );
         games.push(gameInstance);
     }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { serve } from "./serve.js"; // para servir la carpeta ./public con Bun s
 
 const staticFolder = "./src/public";
 
-const games = await loadGames(100);
+const games = await loadGames(5);
 
 const html = renderGames(games);
 await Bun.write(`${staticFolder}/index.html`, html);
